a22c7d5297acd204a5c5f863b70bd27da1ad5b2b
JacopoRaffi <raffijacopo@gmail.com>
2024-01-27 02:09:58 +0100

    fix

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -399,7 +399,8 @@ class NeuralNetwork:
 
         epochs = 0
         exhausting_patience = patience
-        last_error_increase_percentage = 0
+        last_error_increase_percentage = -1
+        training_err = -1
         last_error = np.inf
         new_error = np.inf
         tr_err = np.inf
